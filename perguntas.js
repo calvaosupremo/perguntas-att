69d9c630034dbc91d6845e25b4406dacdd0a0214
calvaosupremo <127514878+calvaosupremo@users.noreply.github.com>
2024-08-06 22:16:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,37 @@
+export const perguntas = [
+    {
+        enunciado: "O Festival de Parintins acontece na cidade de Parintins, no Amazonas, desde o ano de 1965. São três dias consecutivos de desfiles e competição entre os dos dois bois que representam a festa: Boi Garantido e Boi Caprichoso Uma comissão de jurados é responsável por avaliar quesitos como enredo, música e alegoria e dar a nota final. O Festival de Parintins é considerado Patrimônio Cultural do Brasil.?",
+        alternativas: [
+            {
+                texto: "Não conhecia, muito legal! Quero conhecer mais",
+                afirmacao: [
+                    "Não conhecia.",
+                    "Quero conhecer mais."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: [
+                    "Muito interessante.",
+                    "Já conheço e gosto muito."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "A promoção e preservação da cultura brasileira envolvem diversas áreas, como a educação, turismo, artes e patrimônio histórico.",
+        alternativas: [
+            {
+                texto: "Por isso é importante disseminar a cultura brasileira em todas as regiões, mas claro sem se apropriar delas, pois cada cultura tem seus costumes e beleza própria",
+                afirmacao: [
+                    " Muito legal.",
+                    
+                    
+                ]
+            }
+    
+        ]
+    }
+    ]
